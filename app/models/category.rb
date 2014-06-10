4f7a204d4553e4889320491b4aa98dc889f2de04
nychos <nychka08@yandex.ru>
2014-06-10 15:55:40 +0300

    add icons for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,8 @@
 class Category < ActiveRecord::Base
+  attr_accessor :remote_image_url
   has_many :wishes
   belongs_to :user
+  mount_uploader :image, ImageUploader
 
   def self.make(params)
     category = Category.new
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140610094244) do
+ActiveRecord::Schema.define(version: 20140610120630) do
 
   create_table "categories", force: true do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140610094244) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
   create_table "steps", force: true do |t|
